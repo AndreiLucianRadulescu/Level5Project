a21b2969cc92d6b0142a18f8c8f1b6d460955edb
2577007r <2577007r@student.gla.ac.uk>
2024-11-13 23:50:03 +0000

    Modified problem names.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+import sys
 from input_parser import LPParser
 from simplex_solver import SimplexSolver
 
 lp_parser = LPParser()
 # print(lp_parser)
-lp_parser.parse_file("./problems/sample3.lp")
-# print(lp_parser)
+filename = sys.argv[1]
+lp_parser.parse_file(f"./problems/{filename}")
 
-solver = SimplexSolver(pivot_rule="Bland")
+solver = SimplexSolver(pivot_rule="Dantzig")
 
 solver.solve(lp_parser)
