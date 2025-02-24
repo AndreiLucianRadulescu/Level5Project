00ce85e6b0487eb1062c76cc83276eba0cb753aa
AndreiLucianRadulescu <andrei.lucian.radulescu@gmail.com>
2025-02-24 17:32:22 +0000

    Finished two phase simplex, working on multiple examples

--- simplex_solver.py
+++ simplex_solver.py
@@ -49,14 +49,12 @@ class SimplexSolver:
         #   - slack variables, denoted by _sNumber,
         #   - artificial variables (if we need two phase simplex), denoted by _zzzNumber.
         self.all_variables = self.original_variables + [f'_s{i+1}' for i in range(self.num_constraints)] + [f'_zzz{i+1}' for i in range(len(self.negative_rhs_idxs))]
-
         # Now we need to set the objective function (last row in tableau)
         # If we have artificial variables, we need to set the objective function
         # as the preliminary one, asking to maximize -1 * the sum of all artificial variables.
         art_var_added = 0
         if len(self.negative_rhs_idxs) > 0:
             for neg_idx, i in self.negative_rhs_idxs.items():
-                print(neg_idx)
                 tableau[-1, self.num_variables + self.num_constraints + art_var_added] = 1
                 art_var_added += 1
                 tableau[-1] -= tableau[neg_idx]
@@ -76,47 +74,45 @@ class SimplexSolver:
             # We are now done with Phase 1.
             if temp_solution["status"] != "Optimal" or temp_solution["value"] != 0:
                 self.solution = "Infeasible"
-                return {"status": "Infeasible", "value": - np.inf, "num_pivot_steps": num_pivot_steps}
-            print('FINISHED PHASE 1 \n\n\n\n')
-            for row in tableau[-1:,]:
-                print('obj row')
-                print(row, end = ' ')
-                print()
-                print(self.all_variables)
-                print()
-            print('\n\n\n')
+                return {"status": "Infeasible", "value": - np.inf, "num_pivot_steps": temp_solution['num_pivot_steps']}
+            
             # If we get here, then need to proceed to Phase 2.
-            #tableau = np.delete(tableau, np.array([i for i in range(self.num_variables + self.num_constraints, len(self.all_variables))]), axis = 1)
             current_basis = temp_solution["current_basis"]
-            # tableau = np.delete(tableau, [3], axis=1)
-            # current_basis.remove('_s3')
-            print('HERE' + str(current_basis))
+            artificial_var_in_basis = False
+            for basic_variable in current_basis:
+                if basic_variable.startswith('_zzz'):
+                    artificial_var_in_basis = True
+                    break
+            
+            if not artificial_var_in_basis:
+                tableau = np.delete(tableau, np.array([i for i in range(self.num_variables + self.num_constraints, len(self.all_variables))]), axis = 1)
+            else:
+                columns_to_delete = []
+                variables_to_delete = []
+                for idx, value in enumerate(tableau[-1, :self.num_variables + self.num_constraints]):
 
-            tableau[-1] = Fraction(0)
+                    if value > 0 and self.all_variables[idx] not in current_basis:
+                        columns_to_delete.append(idx)
+                        variables_to_delete.append(self.all_variables[idx])
 
+                tableau = np.delete(tableau, columns_to_delete, axis=1)
+                for var in variables_to_delete:
+                    self.all_variables.remove(var)
+            
             for variable, coefficient in lp_parser.obj_function.items():
                 tableau[-1, self.all_variables.index(variable)] = - coefficient
-            tableau = np.delete(tableau, [3, 4], axis=1)
-            self.all_variables.remove('_s1')
-            self.all_variables.remove('_s2')
-            print(self.all_variables)
-            # # We have initialised the new objective function.
-            # # Now we need to make basis variables have only one value of 1 on column.
-            # for idx, basic_variable in enumerate(current_basis):
-            #     basic_variable_idx = self.all_variables.index(basic_variable)
 
-            #     tableau[-1] -= tableau[idx] * tableau[-1, basic_variable_idx]
-            #print(self.ensure_basis(tableau, current_basis))
-            tableau[-1] += 5 * tableau[3]
-            tableau[-1] -= 4 * tableau[0]
-            tableau
-            for row in tableau:
-                for col in row:
-                    print(col, end = ' ')
-                print()
-            print()
-            print(current_basis)
-            print('\n\n\n')
+            # We have initialised the new objective function.
+            # Now we need to make basis variables have only one value of 1 on column.
+            for idx, basic_variable in enumerate(current_basis):
+                basic_variable_idx = self.all_variables.index(basic_variable)
+                if np.sum(tableau[:, basic_variable_idx]) == 1:
+                    continue
+                
+                tableau[-1] -= tableau[idx] * tableau[-1, basic_variable_idx]
+            
+
+            
             return self.solve_tableau(tableau, current_basis)
         else:
             for variable, coefficient in lp_parser.obj_function.items():
@@ -164,14 +160,6 @@ class SimplexSolver:
 
         num_pivot_steps = 0
         while True:
-            for row in tableau:
-                for col in row:
-                    print(col, end = ' ')
-                print()
-            # print(current_basis)
-            # if not self.ensure_basis(tableau, current_basis):
-            #     return 'WRONG'
-            print('\n\n\n\n')
             if tuple(current_basis) in visited_states:
                 print('Cycle detected. Exiting.')
                 return {"status": "Unsolvable (cycles)", "value": - math.inf, "num_pivot_steps": num_pivot_steps, "current_basis": current_basis}
@@ -243,10 +231,3 @@ class SimplexSolver:
             prefix, num = match.groups()
             return (prefix, int(num) if num else 0)  # Convert number to int for proper sorting
         return (var, 0)
-    
-    def ensure_basis(self, tableau, current_basis):
-        for variable in current_basis:
-            if np.sum(tableau[:, self.all_variables.index(variable)]) != 1:
-                return False
-
-        return True
