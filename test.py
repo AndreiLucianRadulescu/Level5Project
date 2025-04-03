6d604095961fe7058a218e5deaefc4b204a14abb
AndreiLucianRadulescu <andrei.lucian.radulescu@gmail.com>
2025-04-03 02:13:04 +0100

    Added finalised work

--- test.py
+++
@@ -1,4 +0,0 @@
-from dense_lp_generator import DenseLPGenerator
-
-dense_lp_generator = DenseLPGenerator(precision = 4, allow_negative_rhs=False)
-dense_lp_generator.generate_dense_lp('large_test.lp', 3000, 3000)
